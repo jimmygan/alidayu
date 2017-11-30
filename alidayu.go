838918cb62a4450b2990cfbb6201a0c43fa8b46d
ganxianzhi <msgxz@qq.com>
2017-11-30 22:55:58 +0800

    提交发送短信功能

--- alidayu.go
+++ alidayu.go
@@ -1,19 +1,20 @@
 package alidayu
 
 import (
-	"net/url"
-	"math/rand"
-	"time"
-	"encoding/json"
-	"strings"
 	"crypto/hmac"
 	"crypto/sha1"
-	"encoding/hex"
 	"encoding/base64"
+	"encoding/json"
+	"encoding/xml"
 	"fmt"
-	"net/http"
+	"io/ioutil"
+	"math/rand"
 	"net"
-	"errors"
+	"net/http"
+	"net/url"
+	"strconv"
+	"strings"
+	"time"
 )
 
 var (
@@ -64,21 +65,19 @@ func InitAPI(accessKeyId, accessKeySecret string) {
 	AccessKeySecret = accessKeySecret
 }
 
-/**
- * SendSms 发送短信
- *
- * @param string signName
- * 		必填, 短信签名，应严格"签名名称"填写，参考：<a href="https://dysms.console.aliyun.com/dysms.htm#/sign">短信签名页</a>
- * @param string templateCode
- * 		必填, 短信模板Code，应严格按"模板CODE"填写, 参考：<a href="https://dysms.console.aliyun.com/dysms.htm#/template">短信模板页</a>
- * (e.g. SMS_0001)
- * @param string phoneNumbers 必填, 短信接收号码 (e.g. 12345678901,13456789011)
- * @param map|nil templateParam
- *    选填, 假如模板中存在变量需要替换则为必填项 (e.g. map("code"=>"12345", "product"=>"阿里通信"))
- * @param string|nil outId [optional] 选填, 发送短信流水号 (e.g. 1234)
- * @param string|nil smsUpExtendCode [optional] 选填，上行短信扩展码（扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段）
- * @return
- */
+// SendSms 发送短信
+//
+// @param string signName
+// 		必填, 短信签名，应严格"签名名称"填写，参考：<a href="https://dysms.console.aliyun.com/dysms.htm#/sign">短信签名页</a>
+// @param string templateCode
+// 		必填, 短信模板Code，应严格按"模板CODE"填写, 参考：<a href="https://dysms.console.aliyun.com/dysms.htm#/template">短信模板页</a>
+// (e.g. SMS_0001)
+// @param string phoneNumbers 必填, 短信接收号码 (e.g. 12345678901,13456789011)
+// @param map|nil templateParam
+//    选填, 假如模板中存在变量需要替换则为必填项 (e.g. map("code"=>"12345", "product"=>"阿里通信"))
+// @param string|nil outId [optional] 选填, 发送短信流水号 (e.g. 1234)
+// @param string|nil smsUpExtendCode [optional] 选填，上行短信扩展码（扩展码字段控制在7位或以下，无特殊需求用户请忽略此字段）
+// @return
 func SendSms(signName, templateCode, phoneNumbers, outId string, templateParam map[string]interface{}, smsUpExtendCode string) (*Result, error) {
 	timestamp := time.Now().UTC().Format("2006-01-02T15:04:05Z")
 	params := url.Values{}
@@ -104,25 +103,31 @@ func SendSms(signName, templateCode, phoneNumbers, outId string, templateParam m
 		params.Add("SmsUpExtendCode", smsUpExtendCode)
 	}
 
-	resp, err := Request(params)
-	fmt.Println(resp)
-	return resp, err
+	return Request(params)
 }
 
-
- // QuerySendDetails 短信发送记录查询
- // @param string phoneNumbers 必填, 短信接收号码 (e.g. 12345678901)
- // @param string sendDate 必填，短信发送日期，格式Ymd，支持近30天记录查询 (e.g. 20170710)
- // @param int32 pageSize 必填，分页大小
- // @param int32 currentPage 必填，当前页码
- // @param string bizId 选填，短信发送流水号 (e.g. abc123)
- // @return
-func QuerySendDetails(phoneNumbers, sendDate, bizId string, pageSize, currentPage int32) {
-
+// QuerySendDetails 短信发送记录查询
+// @param string phoneNumber 必填, 短信接收号码 (e.g. 12345678901)
+// @param string sendDate 必填，短信发送日期，格式Ymd，支持近30天记录查询 (e.g. 20170710)
+// @param int32 pageSize 必填，分页大小
+// @param int32 currentPage 必填，当前页码
+// @param string bizId 选填，短信发送流水号 (e.g. abc123)
+// @return
+func QuerySendDetails(phoneNumber, sendDate, bizId string, pageSize, currentPage int) (*Result, error) {
+	params := url.Values{}
+	params.Add("SendDate", sendDate)
+	params.Add("Action", "SendSms")
+	params.Add("PhoneNumber", phoneNumber)
+	params.Add("PageSize", strconv.Itoa(pageSize))
+	params.Add("CurrentPage", strconv.Itoa(currentPage))
+	if bizId != "" {
+		params.Add("BizId", bizId)
+	}
+	return Request(params)
 }
 
 // Sign 签名
-func Sign(method string,params url.Values) url.Values {
+func Sign(method string, params url.Values) url.Values {
 	params.Add("SignatureMethod", SignatureMethod)
 	params.Add("AccessKeyId", AccessKeyId)
 	params.Add("SignatureVersion", SignatureVersion)
@@ -131,11 +136,11 @@ func Sign(method string,params url.Values) url.Values {
 
 	// hmac签名
 	data := params.Encode()
-	data = strings.ToUpper(method)+ "&/&" + data
-	data = SpecialUrlEncode(data)
+	data = strings.ToUpper(method) + "&%2F&" + SpecialURLEncode(data)
+	// fmt.Println("sign data = ", data)
 	// accessSecret：你的AccessKeyId对应的秘钥AccessSecret，特别说明：POP要求需要后面多加一个“&”字符，即accessSecret + “&”
-	sign := HmacSha1(data, AccessKeySecret + "&")
-	sign = SpecialUrlEncode(sign)
+	sign := HmacSha1(data, AccessKeySecret+"&")
+	// sign = SpecialURLEncode(sign)
 
 	params.Add("Signature", sign)
 	return params
@@ -143,17 +148,15 @@ func Sign(method string,params url.Values) url.Values {
 
 // Request 向网关发起请求
 func Request(params url.Values) (*Result, error) {
-	resp := new(Result)
 	method := "GET"
 	params = Sign(method, params)
 	urlstring := "http://" + host + "/?" + params.Encode()
-	req := http.NewRequest(method, urlstring, nil)
 
 	client := http.Client{
 		Transport: &http.Transport{
 			Dial: func(network, addr string) (net.Conn, error) {
 				deadline := time.Now().Add(25 * time.Second)
-				c, err := net.DialTimeout(network, addr, time.Second * 20)
+				c, err := net.DialTimeout(network, addr, time.Second*20)
 				if err != nil {
 					return nil, err
 				}
@@ -161,6 +164,11 @@ func Request(params url.Values) (*Result, error) {
 				return c, nil
 			},
 		},
+	}
+
+	req, err := http.NewRequest(method, urlstring, nil)
+	if err != nil {
+		return nil, err
 	}
 
 	response, err := client.Do(req)
@@ -171,21 +179,37 @@ func Request(params url.Values) (*Result, error) {
 	defer response.Body.Close()
 	// 请求成功
 	if response.StatusCode != http.StatusOK {
-		return nil, errors.New("http请求失败")
+		body, _ := ioutil.ReadAll(response.Body)
+		return nil, fmt.Errorf("http请求失败, code=%v body=%v", response.StatusCode, string(body))
 	}
 
-	return resp, nil
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = nil
+	resp := new(Result)
+	if Format == "JSON" {
+		err = json.Unmarshal(body, resp)
+		return resp, err
+	} else if Format == "XML" {
+		err = xml.Unmarshal(body, resp)
+	}
+
+	return resp, err
 }
 
-// HmacSha1签名算法
+// HmacSha1 签名算法
 func HmacSha1(data string, key string) string {
 	mac := hmac.New(sha1.New, []byte(key))
 	mac.Write([]byte(data))
 	return base64.StdEncoding.EncodeToString(mac.Sum(nil))
 }
 
-// SpecialUrlEncode 替换特殊字符串
-func SpecialUrlEncode(str string) string {
+// SpecialURLEncode 替换特殊字符串
+func SpecialURLEncode(str string) string {
+	str = url.QueryEscape(str)
 	str = strings.Replace(str, "+", "%20", -1)
 	str = strings.Replace(str, "*", "%2A", -1)
 	str = strings.Replace(str, "%7E", "~", -1)
@@ -196,7 +220,8 @@ func SpecialUrlEncode(str string) string {
 func RandString(length int) string {
 	key := "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 	var bytes = make([]byte, length)
-	rand.Read(bytes)
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	r.Read(bytes)
 	for i, b := range bytes {
 		bytes[i] = key[b%byte(len(key))]
 	}
--- alidayu_test.go
+++ alidayu_test.go
@@ -3,18 +3,19 @@ package alidayu
 import "testing"
 
 const (
-	accessKeyId = "LTAIWbj0RBh04SWj"
-	accessKeySecret = "uA0UNVpOhrc77qiu8LmvTJKeW2NDoB"
+	accessKeyId     = ""
+	accessKeySecret = ""
 )
 
 func init() {
 	InitAPI(accessKeyId, accessKeySecret)
 }
 
-
 // TestSendSms 测试发送短信
 func TestSendSms(t *testing.T) {
 
+	res, err := SendSms("测试", "test", "18628338385", "2", nil, "")
+	t.Errorf("返回参数%v, E=%v", res, err)
 }
 
 // TestQuerySendDetails 测试短信查询
